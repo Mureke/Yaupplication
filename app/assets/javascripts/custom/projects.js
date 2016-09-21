0cdef201521dd6e980c50232092841fd122ca88f
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-09-21 20:19:33 +0300

    Js changes

--- app/assets/javascripts/custom/projects.js
+++ app/assets/javascripts/custom/projects.js
@@ -4,8 +4,15 @@ $(document).ready(function(){
 		var self = $(this);
 		$('.project-desc-wrapper').each(function(){
 			$(this).slideUp(400);
+			$(this).removeClass('open-project');
 		});
-
+		if(self.hasClass('open-project')){
+			self.removeClass('open-project');
+			self.next().slideUp(600);
+		}
+		else{
+		self.addClass('open-project');
 		self.next().slideDown(600);
+		}
 	});
 });
