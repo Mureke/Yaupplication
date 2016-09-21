262ecf546f5cad069ac582476e024a04d4a1e32a
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-09-21 20:52:04 +0300

    gallery changes

--- app/assets/javascripts/custom/projects.js
+++ app/assets/javascripts/custom/projects.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
 	$('.image-wrapper').on('click', function(){
 		event.preventDefault();
+		event.stopPropagation();
 		var self = $(this);
 		$('.project-desc-wrapper').each(function(){
 			$(this).slideUp(400);
-			$(this).removeClass('open-project');
 		});
-		if(self.hasClass('open-project')){
-			self.removeClass('open-project');
+
+		if(self.is('visible')){
 			self.next().slideUp(600);
 		}
 		else{
-		self.addClass('open-project');
-		self.next().slideDown(600);
+			self.next().slideDown(600);
 		}
 	});
 });
