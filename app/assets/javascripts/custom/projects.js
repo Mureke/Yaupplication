9055422690cfd7cad34bafc1263b741ddc22a215
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-09-21 20:06:08 +0300

    Js for gallery

--- app/assets/javascripts/custom/projects.js
+++ app/assets/javascripts/custom/projects.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	console.log('Yolo');
+	$('.image-wrapper').on('click', function(){
+		event.preventDefault();
+		var self = $(this);
+		$('.project-desc-wrapper').each(function(){
+			$(this).slideUp(400);
+		});
+
+		self.next().slideDown(600);
+	});
 });
