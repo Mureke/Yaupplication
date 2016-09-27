0c87060466d5bfb08f4f0c13ab9a7eb7e422ff84
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-09-27 21:01:56 +0300

    Js fixes

--- app/assets/javascripts/custom/projects.js
+++ app/assets/javascripts/custom/projects.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
+
 	$('.image-wrapper').on('click', function(){
 		event.preventDefault();
 		event.stopPropagation();
 		var self = $(this);
-		$('.project-desc-wrapper').each(function(){
-			$(this).slideUp(400);
+		$('.visible-project').each(function(){
+				$(this).next().slideUp(400);
 		});
 
-		if(self.is('visible')){
+		if(self.hasClass('visible-project')){
 			self.next().slideUp(600);
+			self.removeClass('visible-project');
 		}
 		else{
 			self.next().slideDown(600);
+			self.addClass('visible-project');
 		}
 	});
+
 });
